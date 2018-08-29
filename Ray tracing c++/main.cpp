301960cf8e55ef5b213ef0b957de2ae0eb7fc57d
kanguk Seo <tjrkddnr@korea.ac.kr>
2018-08-29 16:28:31 +0900

    set glClear

--- Ray tracing c++/main.cpp
+++ Ray tracing c++/main.cpp
@@ -4,53 +4,41 @@ Ray tracing
 #include <stdio.h>
 #include "Sphere.h"
 #include "gl_helper.h"
+#define DIM 800
 
+using namespace std;
 
-
-GLfloat light_diffuse[] = { 1.0, 1.0, 1.0, 1.0 };  /* White diffuse light. */
-GLfloat light_position[] = { 1.0, 1.0, 1.0, 0.0 };  /* Infinite light location. */
-
-GLfloat s_position[][3] = { { 3.0, 2.0, 0.0 },		/* shpere 1 */
-							{ -3.0, 2.0, 0.0 } };	/* shpere 2 */
-
+vector<Object*> objects;
 
 void display(void)
 {
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	
+	glClear(GL_COLOR_BUFFER_BIT);
+
 	glutSwapBuffers();
 }
 
-void init(void)
+void key(unsigned char key, int x, int y)
 {
-	/* Enable a single OpenGL light. */
-	glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
-	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
-	glEnable(GL_LIGHT0);
-	glEnable(GL_LIGHTING);
-
-	/* Use depth buffering for hidden surface elimination. */
-	glEnable(GL_DEPTH_TEST);
-
-	/* Setup the view */
-	glMatrixMode(GL_PROJECTION);
-	gluPerspective( /* field of view in degree */ 80.0,
-		/* aspect ratio */ 1.0,
-		/* Z near */ 1.0, /* Z far */ 10.0);
-	glMatrixMode(GL_MODELVIEW);
-	gluLookAt(0.0, 5.0, 5.0,  /* eye is at (0,5,5) */
-		0.0, 0.0, 0.0,      /* center is at (0,0,0) */
-		0.0, 1.0, -1.0);      /* up vector */
+	/*
+	ESC 키로 어플리케이션 종료
+	*/
+	switch (key)
+	{
+	case '27':
+		exit(0);
+	}
 }
 
 int main(int argc, char **argv)
 {
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-	glutCreateWindow("Ray  tracing");
-	init();
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutInitWindowSize(DIM, DIM);
+	glutCreateWindow("Ray Tracing");
+
 
 	glutDisplayFunc(display);
+	glutKeyboardFunc(key);
 	glutMainLoop();
 	return 0;             /* ANSI C requires main to return int. */
 }
