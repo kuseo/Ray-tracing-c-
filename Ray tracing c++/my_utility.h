7a13be4637194fb9a8fa06a84fb992757081f8ae
kanguk Seo <tjrkddnr@korea.ac.kr>
2018-09-01 02:06:17 +0900

    implement randomVector function. generate dimentional vector with random values

--- Ray tracing c++/my_utility.h
+++ Ray tracing c++/my_utility.h
@@ -3,5 +3,29 @@
 
 #include <stdlib.h>
 #include <time.h>
+#include "VECTOR3D.h"
+
+void randomVector(VECTOR3D *v)
+{
+	VECTOR3D output;
+
+	srand(time(NULL));
+	output.x = (rand() % 100) / 100.0;
+
+	srand(time(NULL));
+	output.x = (rand() % 100) / 100.0;
+
+	srand(time(NULL));
+	output.x = (rand() % 100) / 100.0;
+
+	*v = output;
+
+	return;
+}
+
+void calculateUp(VECTOR3D input, VECTOR3D *output)
+{
+
+}
 
 #endif
