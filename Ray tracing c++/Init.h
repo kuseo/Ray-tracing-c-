4834f01862fe0edc6beb524ad1a9974fa5d02178
kanguk Seo <tjrkddnr@korea.ac.kr>
2018-09-01 02:57:18 +0900

    create Init.h

---
+++ Ray tracing c++/Init.h
@@ -0,0 +1,16 @@
+#ifndef INIT_H
+#define INIT_H
+
+#include "gl_helper.h"
+#define DIM 800
+
+void Initialize(int argc, char **argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
+	glutInitWindowPosition(0, 0);
+	glutInitWindowSize(DIM, DIM);
+	glutCreateWindow("Ray Tracing");
+}
+
+#endif
