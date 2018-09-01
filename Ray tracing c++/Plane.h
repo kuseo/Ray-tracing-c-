794ace7c614160fa1583bcaee13a9fabea365d24
kanguk Seo <tjrkddnr@korea.ac.kr>
2018-09-02 01:23:57 +0900

    add new member variables and implement constructor

--- Ray tracing c++/Plane.h
+++ Ray tracing c++/Plane.h
@@ -1,4 +1,47 @@
 #ifndef __PLANE_H__
 #define __PLANE_H__
 
+#include "Object.h"
+
+class Plane : public Object
+{
+public:
+
+	/*
+	member variables
+	*/
+	VECTOR3D normal;
+	VECTOR3D dot;
+
+	/*
+	constructor
+	*/
+	Plane() {}
+	Plane(VECTOR3D normal, VECTOR3D dot) : normal(normal), dot(dot) {}
+
+	/*
+	destructor
+	*/
+	virtual ~Plane() {}
+
+	/*
+	member function
+	*/
+	virtual bool hit(Ray r, float *t)
+	{
+
+	}
+
+	virtual VECTOR3D getColor(VECTOR3D point, VECTOR3D light, VECTOR3D ray)
+	{
+
+	}
+
+	virtual VECTOR3D get_normal(VECTOR3D point)
+	{
+	}
+};
+
+	
 #endif //__PLANE_H__
+
