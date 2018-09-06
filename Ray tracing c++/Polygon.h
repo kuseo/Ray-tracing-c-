c854129111aa5115e95106c31450d159d5e377e3
서강욱 <tjrkddnr@korea.ac.kr>
2018-09-06 16:40:44 +0900

    add polygon class members

--- Ray tracing c++/Polygon.h
+++ Ray tracing c++/Polygon.h
@@ -1,5 +1,53 @@
 #ifndef __POLYGON_H__
-#define ____
+#define __POLYGON_H__
 
+#include "Object.h"
+class Polygon : public Object
+{
+public:
+	/*
+	member variables
+	*/
+	VECTOR3D a, b, c;
+	VECTOR3D alpha, beta, gamma;
+
+	/*
+		constructor
+	*/
+	Polygon() {}
+
+	/*
+	destructor
+	*/
+	virtual ~Polygon() {}
+
+	/*
+	member function
+	*/
+	void varicentric(Ray r)
+	{
+
+	}
+
+	virtual bool hit(Ray r, float *t)
+	{
+
+	}
+
+	virtual VECTOR3D getColor(VECTOR3D point, VECTOR3D light, VECTOR3D ray)
+	{
+
+	}
+
+	virtual VECTOR3D get_normal(VECTOR3D point)
+	{
+
+	}
+
+	virtual VECTOR3D get_normal()
+	{
+
+	}
+};
 
 #endif // __POLYGON_H__
