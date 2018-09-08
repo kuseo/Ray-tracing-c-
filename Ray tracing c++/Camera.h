615183419672498c377066b9cc47de38c97fba67
kanguk Seo <tjrkddnr@korea.ac.kr>
2018-09-09 02:09:09 +0900

    add member variable dir, delete center

--- Ray tracing c++/Camera.h
+++ Ray tracing c++/Camera.h
@@ -10,7 +10,7 @@ public:
 	member variables
 	*/
 	VECTOR3D eye;
-	VECTOR3D center;
+	VECTOR3D dir;
 	VECTOR3D up;
 	VECTOR3D right;
 	float fov;
@@ -18,7 +18,7 @@ public:
 	/*
 	constructor
 	*/
-	Camera() : eye(0.0, 0.0, 0.0), center(0.0, 0.0, -1.0) { up = VECTOR3D(0.0, 1.0, 0.0); right = VECTOR3D(1.0, 0.0, 0.0); }
+	Camera() : eye(0.0, 0.0, 0.0), dir(0.0, 0.0, -1.0) { up = VECTOR3D(0.0, 1.0, 0.0); right = VECTOR3D(1.0, 0.0, 0.0); }
 
 	/*
 	destructor
