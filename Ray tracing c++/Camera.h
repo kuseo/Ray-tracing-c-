8f547b065e14045debe556c4bd8ac01c88af9e88
kanguk Seo <tjrkddnr@korea.ac.kr>
2018-09-09 02:00:42 +0900

    create Camera.h

--- Ray tracing c++/Camera.h
+++ Ray tracing c++/Camera.h
@@ -1,10 +1,42 @@
 #ifndef __CAMERA_H__
 #define __CAMERA_H__
 
-class Camera :
+#include "VECTOR3D.h"
+
+class Camera
 {
 public:
+	/*
+	member variables
+	*/
+	VECTOR3D eye;
+	VECTOR3D center;
+	VECTOR3D up;
+	VECTOR3D right;
+	float fov;
 
-}
+	/*
+	constructor
+	*/
+	Camera() : eye(0.0, 0.0, 0.0), center(0.0, 0.0, -1.0) { up = VECTOR3D(0.0, 1.0, 0.0); right = VECTOR3D(1.0, 0.0, 0.0); }
+	Camera(VECTOR3D eye, VECTOR3D center) : eye(eye), center(center) { }
+
+	/*
+	destructor
+	*/
+	~Camera() {}
+
+	/*
+	member function
+	*/
+
+	/*
+	setter
+	*/
+
+	/*
+	getter
+	*/
+};
 
 #endif //__CAMERA_H__
