bd929990fd2210c544317176cd08ec36c136139c
kanguk Seo <tjrkddnr@korea.ac.kr>
2018-09-14 23:23:20 +0900

    constructor overload

--- Ray tracing c++/Camera.h
+++ Ray tracing c++/Camera.h
@@ -14,12 +14,16 @@ public:
 	VECTOR3D up;
 	VECTOR3D right;
 	float speed;
-	float fov;
 
 	/*
 	constructor
 	*/
 	Camera() : pos(0.0f, 0.0f, 0.0f), dir(0.0f, 0.0f, -1.0f), up(0.0f, 1.0f, 0.0f), right(1.0f, 0.0f, 0.0f), speed(2.0f) { }
+	Camera(float px, float py, float pz, float dx, float dy, float dz, float ux, float uy, float uz) :
+		pos(px, py, pz), dir(dx, dy, dz), up(ux, uy, uz)
+	{
+		right = dir.CrossProduct(up);
+	}
 
 	/*
 	destructor
