04a1b406a7106dc7a01a7fe469a176604d2e645d
서강욱 <tjrkddnr@korea.ac.kr>
2018-09-17 15:54:42 +0900

    add constructors

--- Ray tracing c++/Camera.h
+++ Ray tracing c++/Camera.h
@@ -2,6 +2,15 @@
 #define __CAMERA_H__
 
 #include "VECTOR3D.h"
+enum Camera_Movement { FORWARD, BACKWARD, LEFT, RIGHT };
+
+/*
+defluat camera values
+*/
+const float YAW = -90.0f;
+const float PITCH = 0.0f;
+const float SPEED = 1.0f;
+const float SENSITIVITY = 0.1f;
 
 class Camera
 {
@@ -9,20 +18,40 @@ public:
 	/*
 	member variables
 	*/
+	/* cameara attributes */
 	VECTOR3D pos;
-	VECTOR3D dir;
 	VECTOR3D up;
+	VECTOR3D front;
 	VECTOR3D right;
+	VECTOR3D worldup;
+
+	/* euler angles */
+	float yaw;
+	float pitch;
+
+	/* camera options */
 	float speed;
+	float sensitivity;
 
 	/*
 	constructor
 	*/
-	Camera() : pos(0.0f, 0.0f, 0.0f), dir(0.0f, 0.0f, -1.0f), up(0.0f, 1.0f, 0.0f), right(1.0f, 0.0f, 0.0f), speed(1.0f) { }
-	Camera(float px, float py, float pz, float dx, float dy, float dz, float ux, float uy, float uz, float speed) :
-		pos(px, py, pz), dir(dx, dy, dz), up(ux, uy, uz), speed(speed)
+	Camera() :
+		pos(0.0f, 0.0f, 0.0f), up(0.0f, 1.0f, 0.0f), front(0.0f, 0.0f, -1.0f),
+		yaw(YAW), pitch(PITCH)
 	{
-		right = dir.CrossProduct(up);
+		this->worldup = up;
+		updateCameraVectors();
+	}
+	Camera(VECTOR3D pos,VECTOR3D up, float yaw, float pitch) :
+		front(0.0f, 0.0f, -1.0f), 
+		speed(SPEED), sensitivity(SENSITIVITY)
+	{
+		this->pos = pos;
+		this->worldup = up;
+		this->yaw = yaw;
+		this->pitch = pitch;
+		updateCameraVectors();
 	}
 
 	/*
@@ -33,6 +62,18 @@ public:
 	/*
 	member function
 	*/
+	void updateCameraVectors()
+	{
+		VECTOR3D front;
+		front.x = cos(radian(yaw)) * cos(radian(pitch));
+		front.y = sin(radian(pitch));
+		front.z = sin(radian(yaw)) * cos(radian(pitch));
+		front.Normalize();
+		right = front.CrossProduct(worldup);
+		right.Normalize();
+		up = right.CrossProduct(front);
+		up.Normalize();
+	}
 
 	/*
 	setter
